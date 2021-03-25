5eca637972bfb7b958feaa6066f2a73a73f2479b
VinceBarnachea <barnacheatolentino@gmail.com>
2021-03-25 14:26:34 +0800

    Added Payment Method

--- app/src/main/java/com/example/onlineshapp/Checkout.java
+++ app/src/main/java/com/example/onlineshapp/Checkout.java
@@ -1,14 +1,33 @@
 package com.example.onlineshapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 
-public class Checkout extends AppCompatActivity {
+public class Checkout extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout);
+
+        Spinner paymentMethod = findViewById(R.id.payment_method);
+        paymentMethod.setOnItemSelectedListener(this);
+    }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
